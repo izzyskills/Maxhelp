c6c675fe5961ab39fab09bb30817c1010e4d95df
Israel Omola <izzyskills@gmail.com>
2024-12-01 21:05:39 +0100

    fixed jwt

--- backend/app/utils/utils.py
+++ backend/app/utils/utils.py
@@ -1,27 +1,24 @@
 from passlib.context import CryptContext
-from jwt.exceptions import PyJWTError
-from jose import jwt, JWTError
+from jwt.exceptions import PyJWTError as JWTError
+from jwt import PyJWT
 from datetime import datetime, timedelta
 from db.session import settings
 from typing import Optional
 from fastapi.security import OAuth2PasswordBearer
 
-
+jwt = PyJWT()
 
 # OAuth2 scheme for extracting token
 oauth2_scheme_user = OAuth2PasswordBearer(tokenUrl="auth/admin/login/")
 
-
-
-
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 def hash_password(password: str) -> str:
     """
     Hash a plain text password using bcrypt.
     """
     return pwd_context.hash(password)
-
 
 
 # Verify password
@@ -37,11 +34,14 @@ def create_access_token(data: dict, expires_delta: timedelta) -> str:
     # Use jwt.encode directly
     return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
 
+
 # Verify access token
 def verify_access_token(token: str) -> Optional[dict]:
     try:
         # Use jwt.decode directly
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
         return payload  # Decoded token payload
     except JWTError:
         return None
